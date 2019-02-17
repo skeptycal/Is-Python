9c84b7b9af4862020bff48578537566152c219b4
skeptycal <skeptycal@gmail.com>
2019-02-16 18:22:38 -0600

    details ... details

--- is_python.py
+++ is_python.py
@@ -1,8 +1,61 @@
 #!/usr/bin/env python3
-'''############################################################################
-    python_shell.py - Python3 returns a string containing the parent shell
+'''
+###############################################################################
+   python_shell.py - Python3 returns a string containing the parent shell
 
+###############################################################################
+    requires no parameters
+
+    returns
+    'shell' (started python on command line using "python")
+    'ipython' (started ipython on command line using "ipython")
+    'ipython-notebook' (e.g., running in Spyder or started with "ipython qtconsole")
+    'Jupyter Notebook' (running in a Jupyter notebook)
+    
     Copyright (C) 2019  Michael Treanor <https://www.github.com/skeptycal>
+    License GPL3.0
+
+###############################################################################
+'''
+def python_shell():
+    import os
+    try:
+        import platform
+    except ImportError:
+        im_plat = False
+    else:
+        im_plat = True
+
+    env = os.environ
+    shell = 'shell'
+    program = os.path.basename(env['_'])
+
+    if 'jupyter-notebook' in program:
+        shell = 'Jupyter Notebook'
+    elif 'JPY_PARENT_PID' in env:
+        shell = 'ipython-notebook'
+    elif 'ipython' in program:
+        shell = "ipython"
+    elif im_plat:
+        shell = platform.python_implementation()
+    return shell
+
+# ! TEST - REMOVE THIS
+print("The type of python shell you are using is: ", python_shell(), ".")
+
+
+'''############################################################################
+Testing complete 2/14/19
+    macOS Mojave       10.14.3
+    jupyter --version    4.4.0
+    ipython --version    7.2.0
+    spyder(anaconda)     3.3.2
+    python --version     3.7.2
+    PyPy3 --version      6.0.0
+############################################################################'''
+
+'''############################################################################
+License: GPL3.0
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,50 +69,20 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ############################################################################'''
-import os
 
-
-def python_shell():
-
-    env = os.environ
-    shell = 'shell'
-    program = os.path.basename(env['_'])
-
-    if 'jupyter-notebook' in program:
-        shell = 'jupyter-notebook'
-    elif 'ipython' in program:
-        shell = "ipython"
-    elif 'JPY_PARENT_PID' in env:
-        shell = 'ipython-notebook'
-    return shell
-
-
-# ! TEST
-# ! REMOVE THIS
-print("The type of python shell you are using is: ", python_shell(), ".")
-
-''' python_shell() returns
-
-    'shell' (started python on command line using "python")
-    'ipython' (started ipython on command line using "ipython")
-    'ipython-notebook' (e.g., running in Spyder or started with "ipython qtconsole")
-    'jupyter-notebook' (running in a Jupyter notebook)
-'''
-
-''' Modifications: 
-
-    * The logic was clarified (removed double check for JPY_PARENT_PID in env)
+'''############################################################################
+Modifications: 
+    * The logic in the main example was clarified 
+      (removed double check for JPY_PARENT_PID in env)
     * "import os" placed outside of the function body since it will always be
        required by this module and highly likely will be required by any program
        that is concerned with which shell called it.
 
-    Sources:
-
-    * Weigel, Bob, <post reply>,https://stackoverflow.com/a/53436734/9878098, accessed 2/13/19
+Research sources:
+    * https://stackoverflow.com/a/53436734/9878098
       Quote: "The following captures the cases of https://stackoverflow.com/a/50234148/1491619 
       without needing to parse the output of ps" 
       -- Bob Weigel (https://stackoverflow.com/users/1491619/bob-weigel)
-    * See also https://stackoverflow.com/a/37661854
-'''
+    * See also https://stackoverflow.com/a/37661854 
+############################################################################'''
